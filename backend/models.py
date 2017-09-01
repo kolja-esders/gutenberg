81a5d50f70bf2a337ec0e08ad6b9a26a78fccea7
Kolja Esders <kolja.esders@gmail.com>
2017-09-02 00:32:12 +0200

    First models

--- backend/models.py
+++ backend/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    books = models.ManyToManyField('self', through='UserBookJoin', symmetrical=False)
 
 class Book(models.Model):
     name = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
+
+class UserBookJoin(models.Model):
+    user = models.ForeignKey(User)
+    book = models.ForeignKey(Book)
+    state = models.CharField(max_length = 31) # to-read, read, ...
+    rating = models.PositiveSmallIntegerField
