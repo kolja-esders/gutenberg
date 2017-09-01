384bb375b5153db80b4002ff9e0ea78f9c624ba4
Kolja Esders <kolja.esders@gmail.com>
2017-09-01 23:43:46 +0200

    Added a custom user

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
