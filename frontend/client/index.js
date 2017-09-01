b7b494e13ce5dd28f5bff7f5f115f91a517f3190
gitexa <uafla@student.kit.edu>
2017-09-02 01:01:57 +0200

    basic react functionality with babel and webpack

---
+++ frontend/client/index.js
@@ -0,0 +1,12 @@
+/*
+    ./client/index.js
+    which is the webpack entry file
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+//import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+console.log('Hey guys and ladies!!')
