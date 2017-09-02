2a62db4212e7e5aea137b433d4bcafc0b9a517ab
Kolja Esders <kolja.esders@gmail.com>
2017-09-02 17:11:55 +0200

    Routes scaffolding

--- frontend/client/components/Main.jsx
+++ frontend/client/components/Main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Landing from './Landing.jsx'
+import Home from './Home.jsx'
 
 export default class Main extends React.Component {
   render() {
     return (
-        <div>hiiii</div>
+      <main>
+        <Switch>
+          <Route exact path='/' component={Landing}/>
+          <Route path='/home' component={Home}/>
+          <Route path='/all-books' component={Home}/>
+        </Switch>
+      </main>
     )
   }
 }
