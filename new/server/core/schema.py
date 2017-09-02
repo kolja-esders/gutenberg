42ac90e045e5d6c1ef2b2ab84e5c09f82c332253
Oruluk <pat.dein@gmail.com>
2017-09-03 00:37:27 +0200

    Graphene Models

--- new/server/core/schema.py
+++ new/server/core/schema.py
@@ -3,23 +3,81 @@ from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 
 from users.jwt_util import get_token_user_id
-# from .models import Question as QuestionModal, Choice as ChoiceModal, \
-    # Vote as VodeModal
+from .models import Book as BookModal, UserBookJoin as UserBookJoinModal
+
+
+class Book(DjangoObjectType):
+    class Meta:
+        model = BookModal
+
+class UserBookJoin(DjangoObjectType):
+    class Meta:
+        model = UserBookJoinModal
 
 
 class CoreQueries(graphene.AbstractType):
-    # question = graphene.Node.Field(Question)
-    # questions = DjangoFilterConnectionField(Question)
+    books = graphene.Node.Field(Book)
+    #books = DjangoFilterConnectionField(Book)
 
-    def resolve_questions(self, args, context, info):
-        pass
-        # issues = QuestionModal.objects
-        # order_by = args.get('order_by')
-        # if order_by:
-            # issues.order_by(order_by)
+    user_book_joins = graphene.Node.Field(UserBookJoin)
+    #user_book_joins = DjangoFilterConnectionField(UserBookJoin)
 
-        # return issues
+    def resolve_books(self, args, context, info):
+        books = BookModal.objects
+        return books
+
+    def resolve_user_book_joins(self, args, context, info):
+        user_book_joins = UserbookJoinModal.objects
+        return user_book_joins
+
+class BookInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    author = graphene.String(required=True)
+    publishing_year = graphene.Int(required=True)
+
+
+class CreateBook(graphene.Mutation):
+    class Input:
+        book_data = BookInput(required=True)
+
+    book = graphene.Field(Book)
+
+    def mutate(self, args, ctx, info):
+        book_data = args['book_data']
+        book = Book(
+                name = book_data['name'],
+                author = book_data['author']
+            )
+        book.save()
+        return CreateBook(book=book)
+
+
+class ConnectUserToBook(graphene.Mutation):
+    class Input:
+        user_id = graphene.String(required=True)
+        book_id = graphene.String(required=True)
+        state = graphene.String(required=True)
+        rating = graphene.Int(required=True)
+
+    user_book_join = graphene.Field(UserBookJoin)
+
+    def mutate(self, args, ctx, info):
+        user_id = args['user_id']
+        book_id = args['book_id']
+        state = args['state']
+        rating = args['rating']
+        user = User.objects.get(pk=user_id)
+        book = Book.objects.get(pk=book_id)
+        user_book_join = UserBookJoin(
+                user = user,
+                book= book,
+                state = state,
+                rating = rating
+            )
+        user_book_join.save()
+        return ConnectUserToBook(user_book_join=user_book_join)
+
 
 class CoreMutations(graphene.AbstractType):
-    pass
-    # vote = VoteMutation.Field()
+    create_book = CreateBook.Field()
+    create_user_to_book = ConnectUserToBook.Field()
