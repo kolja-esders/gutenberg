ad3b50c2bee34f6b3f273e5fc06074473595f840
Kolja Esders <kolja.esders@gmail.com>
2017-09-05 04:38:10 +0200

    Loading all books with loading indicator

--- client/components/SharedBooksList/SharedBooksList.js
+++ client/components/SharedBooksList/SharedBooksList.js
@@ -2,35 +2,86 @@ import React from 'react';
 import { graphql, createFragmentContainer, QueryRenderer } from 'react-relay';
 import Page from 'components/Page/Page';
 import { authenticatedRoute } from 'modules/auth/utils'
-import { Table } from 'semantic-ui-react';
+import { Table, Dimmer, Loader } from 'semantic-ui-react';
 import styles from './SharedBooksList.scss';
+import { environment } from '../../utils/relay'
 
 class SharedBookList extends React.Component {
   render() {
     const book_entries = this.props.book_entries;
     return (
       <div className={styles.root}>
-        <Table singleLine className={styles.books}>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Title</Table.HeaderCell>
-              <Table.HeaderCell>Author</Table.HeaderCell>
-              <Table.HeaderCell>Rating</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
 
-          <Table.Body>
+            <QueryRenderer
+              environment={environment}
+              query={graphql`
+                query SharedBooksList_AllUserBookJoinsQuery {
+                  userBookJoins {
+                    id
+                    book {
+                      name
+                      author
+                    }
+                    user {
+                      firstName
+                    }
+                    rating
+                    state
+                  }
+                }
+              `}
+              render={({error, props}) => {
+                if (error) {
+                  return (
+                    <Table singleLine className={styles.books}>
+                      <Table.Body>
+                        <Table.Cell width={16}>
+                          <Loader active inline='centered'/>
+                        </Table.Cell>
+                      </Table.Body>
+                    </Table>
+                  )
+                } else if (props) {
+                  return (
+                    <Table singleLine className={styles.books}>
+                      <Table.Header>
+                        <Table.Row>
+                          <Table.HeaderCell>Title</Table.HeaderCell>
+                          <Table.HeaderCell>Author</Table.HeaderCell>
+                          <Table.HeaderCell>Rating</Table.HeaderCell>
+                        </Table.Row>
+                      </Table.Header>
+                      <Table.Body>
+                        {props.userBookJoins.map((e) =>
+                          <Table.Row key={e.id}>
+                            <Table.Cell>{e.book.name}</Table.Cell>
+                            <Table.Cell>{e.book.author}</Table.Cell>
+                            <Table.Cell>{e.rating}</Table.Cell>
+                          </Table.Row>
+                        )}
+                      </Table.Body>
+                    </Table>
+                  )
+                }
+                return (
+                  <Table singleLine className={styles.books}>
+                    <Table.Header>
+                      <Table.Row>
+                        <Table.HeaderCell></Table.HeaderCell>
+                      </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                      <Table.Row>
+                        <Table.Cell width={16}>
+                          <Loader active inline='centered'/>
+                        </Table.Cell>
+                      </Table.Row>
+                    </Table.Body>
+                  </Table>
+                );
+              }}
+            />
 
-            {book_entries.map((e) =>
-              <Table.Row id={e.id}>
-                <Table.Cell>{e.book.name}</Table.Cell>
-                <Table.Cell>{e.book.author}</Table.Cell>
-                <Table.Cell>{e.rating}</Table.Cell>
-              </Table.Row>
-            )}
-
-          </Table.Body>
-        </Table>
       </div>
     );
   }
--- client/modules/core/SharedBooks/SharedBooks.js
+++ client/modules/core/SharedBooks/SharedBooks.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/href-no-hash */
 import React from 'react';
-import { graphql, createFragmentContainer } from 'react-relay';
+import { graphql, createFragmentContainer, QueryRenderer } from 'react-relay';
 import Page from 'components/Page/Page';
 import SharedBooksList from 'components/SharedBooksList/SharedBooksList';
 import { authenticatedRoute } from 'modules/auth/utils'
@@ -8,10 +8,10 @@ import { authenticatedRoute } from 'modules/auth/utils'
 class SharedBooks extends React.Component {
   render() {
     return (
-        <div>
-      <Page title='Books' viewer={this.props.viewer}>
+      <div>
+        <Page title='Books' viewer={this.props.viewer}>
           <SharedBooksList book_entries={this.props.viewer.user.books}/>
-      </Page>
+        </Page>
       </div>
     );
   }
