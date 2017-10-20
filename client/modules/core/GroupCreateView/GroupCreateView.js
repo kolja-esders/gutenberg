705a1705e28641f84d95bf53c032c270e092a181
Kolja Esders <kolja.esders@gmail.com>
2017-10-20 20:48:59 +0200

    Smaller fixes for group create view

--- client/modules/core/GroupCreateView/GroupCreateView.js
+++ client/modules/core/GroupCreateView/GroupCreateView.js
@@ -1,41 +1,57 @@
 /* eslint-disable jsx-a11y/href-no-hash */
-import React from 'react'
-import { graphql, createFragmentContainer } from 'react-relay'
-import { withAuth } from 'modules/auth/utils'
-import Page from 'components/Page/Page'
-import { Button, Input, Header } from 'semantic-ui-react'
-import { Link } from 'found'
+import React from 'react';
+import { graphql, createFragmentContainer } from 'react-relay';
+import { withAuth } from 'modules/auth/utils';
+import Page from 'components/Page/Page';
+import { Button, Input } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
-import styles from './GroupCreateView.scss'
+import styles from './GroupCreateView.scss';
+
+const CreateGroupMutation = graphql`
+  mutation GroupCreateViewMutation (
+    $name: String!
+    $nameUrl: String!
+  ) {
+    createGroup(name: $name, nameUrl: $nameUrl) {
+      group {
+        id
+      }
+    }
+  }
+`;
 
 class GroupCreateView extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {url: ''}
+  static propTypes = {
+    viewer: PropTypes.object.isRequired
   }
-  
-  nameToUrl = (name) => {
-    return name.toLowerCase().replace(/ /g,'-')
-  }
+
+  state = { url: '' }
 
   onChangeHandler = (ev, data) => {
-    this.setState({url: this.nameToUrl(data.value)})
+    this.setState({ url: this.nameToUrl(data.value) });
   }
 
+  onSubmitHandler = (ev, data) => {
+    ev.preventDefault();
+    console.log(ev, data);
+  }
+
+  nameToUrl = name => name.toLowerCase().replace(/ /g, '-')
+
   render() {
-    const group = this.props.viewer.group
     return (
       <Page title='Gutenberg' viewer={this.props.viewer}>
-        <section className = {styles.container}>
-          <form className={styles.createForm}>
-            <Input className={styles.groupNameInput} size='massive' onChange={this.onChangeHandler}/>
+        <section className={styles.container}>
+          <form className={styles.createForm} onSubmit={this.onSubmitHandler}>
+            <Input className={styles.groupNameInput} size='massive' onChange={this.onChangeHandler} />
             <p className={styles.preview}>gutenberg.de/group/<span>{ this.state.url }</span></p>
 
             <Button className={styles.submitButton} type='submit' size='massive' primary fluid>Create group</Button>
           </form>
         </section>
       </Page>
-    )
+    );
   }
 }
 
