b7c0fc449f617cc74e075a2276755b0d370e059e
Kolja Esders <kolja.esders@gmail.com>
2017-10-20 03:21:44 +0200

    Group view page draft

--- client/modules/core/GroupView/GroupView.js
+++ client/modules/core/GroupView/GroupView.js
@@ -1,27 +1,30 @@
 /* eslint-disable jsx-a11y/href-no-hash */
-import React from 'react';
-import { graphql, createFragmentContainer } from 'react-relay';
-import SharedBooksList from 'components/SharedBooksList/SharedBooksList';
+import Page from 'components/Page/Page'
+import SharedBooksList from 'components/SharedBooksList/SharedBooksList'
 import { withAuth } from 'modules/auth/utils'
+import React from 'react'
+import { createFragmentContainer, graphql } from 'react-relay'
+
+import styles from './GroupView.scss'
 
 class GroupView extends React.Component {
   render() {
     const group = this.props.viewer.group
     return (
-      <div>
-        You're a member of group { group.name }.
-      </div>
+      <Page title='Gutenberg' viewer={this.props.viewer} activeGroup={group.name}>
+        <section className = {styles.container}>
+          You're a member of group { group.name }.
+        </section>
+      </Page>
     )
   }
 }
 
-export default createFragmentContainer(
-  withAuth(GroupView),
-  graphql`
+export default createFragmentContainer(withAuth(GroupView), graphql`
     fragment GroupView_viewer on Viewer {
+      ...Page_viewer
       group(nameUrl: $nameUrl) {
         name
       }
     }
-  `
-);
+  `);
