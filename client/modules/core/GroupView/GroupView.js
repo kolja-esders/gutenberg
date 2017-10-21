b1a84ce967fb4deb5f3ed8dbed19f2e76f0fb782
Kolja Esders <kolja.esders@gmail.com>
2017-10-21 23:26:51 +0200

    Friend invite nudge for groups

--- client/modules/core/GroupView/GroupView.js
+++ client/modules/core/GroupView/GroupView.js
@@ -1,10 +1,10 @@
-/* eslint-disable jsx-a11y/href-no-hash */
 import Page from 'components/Page/Page';
 import { withAuth } from 'modules/auth/utils';
 import React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
 import PropTypes from 'prop-types';
 import { Link } from 'found';
+import { Button, Header } from 'semantic-ui-react';
 
 import styles from './GroupView.scss';
 
@@ -14,14 +14,19 @@ class GroupView extends React.Component {
   }
 
   render() {
-    const group = this.props.viewer.group;
+    const { group, user } = this.props.viewer;
     const inviteLink = `/group/${group.nameUrl}/invite`;
+    const memberNodes = group.members.edges;
 
     return (
       <Page title='Gutenberg' viewer={this.props.viewer} activeGroup={group.name}>
         <section className={styles.container}>
-          You are a member of group { group.name }.
-          <Link to={inviteLink}>Invite friends</Link>
+          { memberNodes.length === 1 &&
+          <div className={styles.inviteNudge}>
+            <Header className={styles.text} size='huge'>{user.firstName}, it's only you.<span className={styles.emoji}></span></Header>
+            <Button className={styles.btn} size='massive' as={Link} to={inviteLink} primary>Invite friends</Button>
+          </div>
+          }
         </section>
       </Page>
     );
@@ -31,9 +36,19 @@ class GroupView extends React.Component {
 export default createFragmentContainer(withAuth(GroupView), graphql`
     fragment GroupView_viewer on Viewer {
       ...Page_viewer
+      user {
+        firstName
+      }
       group(nameUrl: $nameUrl) {
         name
         nameUrl
+        members {
+          edges {
+            node {
+              firstName
+            }
+          }
+        }
       }
     }
   `);
