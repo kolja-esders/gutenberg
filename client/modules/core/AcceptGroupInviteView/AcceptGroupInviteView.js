ff807e673429fa2702eb2947abc39719a0533876
Kolja Esders <kolja.esders@gmail.com>
2017-12-08 20:01:32 +0100

    Show error when invite has been consumed

--- client/modules/core/AcceptGroupInviteView/AcceptGroupInviteView.js
+++ client/modules/core/AcceptGroupInviteView/AcceptGroupInviteView.js
@@ -3,7 +3,7 @@ import Page from 'components/Page/Page';
 import React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
 import PropTypes from 'prop-types';
-import { Button, Input, Grid } from 'semantic-ui-react';
+import { Button, Input, Grid, Message } from 'semantic-ui-react';
 
 import styles from './AcceptGroupInviteView.scss';
 
@@ -36,77 +36,82 @@ class AcceptGroupInviteView extends React.Component {
 
   render() {
     const { input } = this.state;
+    const invite = this.props.viewer.groupInvite;
 
     return (
       <Page viewer={this.props.viewer} title="Accept invite">
         <div className={styles.container}>
-          <form
-            id="accept_invite"
-            onSubmit={this.submitForm}
-            className={styles.form}
-          >
-            <Grid className={styles.nameFields}>
-              <Grid.Row columns={2} className={styles.row}>
-                <Grid.Column className={styles.column}>
-                  <Input
-                    id='firstName'
-                    onChange={this.handleFieldChange.bind(this)}
-                    className={styles.nameField}
-                    value={input.firstName}
-                    type='text'
-                    size='huge'
-                    fluid
-                    required
-                    placeholder='First name' />
-                </Grid.Column>
-                <Grid.Column className={styles.column}>
-                  <Input
-                    id='lastName'
-                    onChange={this.handleFieldChange.bind(this)}
-                    className={styles.nameField}
-                    value={input.lastName}
-                    type='text'
-                    size='huge'
-                    fluid
-                    required
-                    placeholder='Last name' />
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-
-            <Input
-              id='email'
-              className={`${styles.textFields} email_input`}
-              onChange={this.handleFieldChange}
-              value={input.email}
-              type='email'
-              size='huge'
-              required
-              placeholder='Email' />
-
-            <br />
-
-            <Input
-              id='password'
-              className={styles.textFields}
-              onChange={this.handleFieldChange.bind(this)}
-              placeholder='Password'
-              type='password'
-              size='huge'
-              minLength={8}
-              required
-            />
-
-            <Button
-              color='green'
-              fluid
-              type='submit'
-              size='huge'
-              className='button_submit-signup-form'
+          { invite.consumed ?
+            <Message size="massive" negative>The invite has already been accepted.</Message>
+          :
+            <form
+              id="accept_invite"
+              onSubmit={this.submitForm}
+              className={styles.form}
             >
-              Join
-            </Button>
-          </form>
+              <Grid className={styles.nameFields}>
+                <Grid.Row columns={2} className={styles.row}>
+                  <Grid.Column className={styles.column}>
+                    <Input
+                      id='firstName'
+                      onChange={this.handleFieldChange.bind(this)}
+                      className={styles.nameField}
+                      value={input.firstName}
+                      type='text'
+                      size='huge'
+                      fluid
+                      required
+                      placeholder='First name' />
+                  </Grid.Column>
+                  <Grid.Column className={styles.column}>
+                    <Input
+                      id='lastName'
+                      onChange={this.handleFieldChange.bind(this)}
+                      className={styles.nameField}
+                      value={input.lastName}
+                      type='text'
+                      size='huge'
+                      fluid
+                      required
+                      placeholder='Last name' />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+
+              <Input
+                id='email'
+                className={`${styles.textFields} email_input`}
+                onChange={this.handleFieldChange}
+                value={input.email}
+                type='email'
+                size='huge'
+                required
+                placeholder='Email' />
+
+              <br />
+
+              <Input
+                id='password'
+                className={styles.textFields}
+                onChange={this.handleFieldChange.bind(this)}
+                placeholder='Password'
+                type='password'
+                size='huge'
+                minLength={8}
+                required
+              />
+
+              <Button
+                color='green'
+                fluid
+                type='submit'
+                size='huge'
+                className='button_submit-signup-form'
+              >
+                Join
+              </Button>
+            </form>
+          }
         </div>
       </Page>
     )
