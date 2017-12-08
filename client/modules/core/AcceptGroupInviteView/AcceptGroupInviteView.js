1d553f41ff7505f16ef48c35409adf4396c21701
Kolja Esders <kolja.esders@gmail.com>
2017-12-08 18:52:51 +0100

    Prepopulated form for accepting invite

--- client/modules/core/AcceptGroupInviteView/AcceptGroupInviteView.js
+++ client/modules/core/AcceptGroupInviteView/AcceptGroupInviteView.js
@@ -4,45 +4,110 @@ import { withAuth } from 'modules/auth/utils';
 import React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
 import PropTypes from 'prop-types';
-import { Button, Input, Icon } from 'semantic-ui-react';
+import { Button, Input, Grid } from 'semantic-ui-react';
 
-import styles from './AcceptInviteView.scss';
+import styles from './AcceptGroupInviteView.scss';
 
-class AcceptInviteView extends React.Component {
+class AcceptGroupInviteView extends React.Component {
   static propTypes = {
     viewer: PropTypes.object.isRequired
-  }
-
-  onChangeHandler = (ev, { value }) => {
   }
 
   onSubmitHandler = (ev) => {
     ev.preventDefault();
   }
 
-  render() {
-    const group = this.props.viewer.group;
-    return (
-      <Page title='Gutenberg' viewer={this.props.viewer} activeGroup={group.name}>
-        <section className={styles.container}>
-          <form className={styles.createForm} onSubmit={this.onSubmitHandler}>
-            <Input className={styles.friendEmailInput} placeholder='john@doe.com' size='massive' onChange={this.onChangeHandler} />
-            <Input className={styles.friendEmailInput} placeholder='john@doe.com' size='massive' onChange={this.onChangeHandler} />
-            <Input className={styles.friendEmailInput} placeholder='john@doe.com' size='massive' onChange={this.onChangeHandler} />
+  handleFieldChange = (e) => {
+    const input = this.state.input
+    const inputName = e.target.id
+    input[inputName] = e.target.value
+    this.setState({ input })
+  }
 
-            <Button className={styles.submitButton} type='submit' size='massive' primary fluid><Icon name='send' />Send invites</Button>
+  render() {
+    let input = { firstName: 'John', lastName: 'Doe', email: 'john.doe@gmail.com' }
+    return (
+      <Page viewer={this.props.viewer} title="Accept invite">
+        <div className={styles.container}>
+          <form
+            id="accept_invite"
+            onSubmit={this.submitForm}
+            className={styles.form}
+          >
+            <Grid className={styles.nameFields}>
+              <Grid.Row columns={2} className={styles.row}>
+                <Grid.Column className={styles.column}>
+                  <Input
+                    id='firstName'
+                    className={styles.nameField}
+                    value={input.firstName}
+                    type='text'
+                    size='huge'
+                    fluid
+                    required
+                    placeholder='First name' />
+                </Grid.Column>
+                <Grid.Column className={styles.column}>
+                  <Input
+                    id='lastName'
+                    className={styles.nameField}
+                    value={input.lastName}
+                    type='text'
+                    size='huge'
+                    fluid
+                    required
+                    placeholder='Last name' />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+
+            <Input
+              id='email'
+              className={`${styles.textFields} email_input`}
+              onChange={this.handleFieldChange}
+              value={input.email}
+              disabled
+              type='email'
+              size='huge'
+              required
+              placeholder='Email' />
+
+            <br />
+
+
+            <Input
+              id='password'
+              className={styles.textFields}
+              onChange={this.handleFieldChange.bind(this)}
+              value={input.password}
+              placeholder='Password'
+              type='password'
+              size='huge'
+              minLength={8}
+              required
+            />
+
+            <Button
+              color='green'
+              fluid
+              type='submit'
+              size='huge'
+              className='button_submit-signup-form'
+            >
+              Join
+            </Button>
           </form>
-        </section>
+        </div>
       </Page>
-    );
+    )
   }
 }
 
-export default createFragmentContainer(withAuth(GroupInviteView), graphql`
-    fragment GroupInviteView_viewer on Viewer {
+export default createFragmentContainer(AcceptGroupInviteView, graphql`
+    fragment AcceptGroupInviteView_viewer on Viewer {
       ...Page_viewer
-      group(nameUrl: $nameUrl) {
-        name
+      groupInvite(verificationToken: $verificationToken) {
+        email
       }
     }
   `);
