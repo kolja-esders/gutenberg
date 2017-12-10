c6d613b825957499a60342eb22905aaee38339ae
Kolja Esders <kolja-esders@users.noreply.github.com>
2017-12-10 20:48:41 +0100

    Added created_at & updated_at to all entities (#20)

--- server/core/models.py
+++ server/core/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from custom_user.models import AbstractEmailUser
+from django.utils import timezone
+
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return datetime.datetime.now()
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = AutoDateTimeField(default=timezone.now)
 
     class Meta:
         unique_together = ('title', 'author')
@@ -16,6 +23,8 @@ class Book(models.Model):
 class Group(models.Model):
     name = models.CharField(max_length=32, unique=True)
     name_url = models.CharField(max_length=64, unique=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = AutoDateTimeField(default=timezone.now)
 
     @staticmethod
     def get_url_from_name(name):
@@ -27,12 +36,17 @@ class CustomUser(AbstractEmailUser):
     last_name = models.CharField(max_length=31, blank=True)
     groups = models.ManyToManyField(Group, through='Membership', symmetrical=False, related_name='members')
     books = models.ManyToManyField(Book, through='BookshelfEntry', symmetrical=False, related_name='users')
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = AutoDateTimeField(default=timezone.now)
+
 
 class BookshelfEntry(models.Model):
     user = models.ForeignKey(CustomUser)
     book = models.ForeignKey(Book)
     state = models.CharField(max_length=31) # to-read, read, ...
     rating = models.PositiveSmallIntegerField(null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = AutoDateTimeField(default=timezone.now)
 
     class Meta:
         unique_together = ('user', 'book')
@@ -46,6 +60,8 @@ class GroupInvite(models.Model):
     created_by = models.ForeignKey(CustomUser, default=None)
     consumed = models.BooleanField(default=False)
     email_sent = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = AutoDateTimeField(default=timezone.now)
 
     class Meta:
         unique_together = ('email', 'group')
@@ -54,6 +70,8 @@ class Membership(models.Model):
     user = models.ForeignKey(CustomUser)
     group = models.ForeignKey(Group)
     invite = models.ForeignKey(GroupInvite, default=None, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = AutoDateTimeField(default=timezone.now)
 
     class Meta:
         unique_together = ('user', 'group')
