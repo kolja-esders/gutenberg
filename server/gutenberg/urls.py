bfdbd1e970c0b27ce53d8875013bbd0d612a4608
Kolja Esders <kolja.esders@gmail.com>
2018-03-13 20:52:26 +0100

    Migrated image stack to S3

--- server/gutenberg/urls.py
+++ server/gutenberg/urls.py
@@ -19,11 +19,14 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
 from core.views import ProfileImageUploadView
+from core.views import sign_s3_upload, register_profile_image
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     url(r'^upload', csrf_exempt(ProfileImageUploadView.as_view())),
+    url(r'^s3/sign', sign_s3_upload),
+    url(r'^s3/register', register_profile_image),
     url(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
