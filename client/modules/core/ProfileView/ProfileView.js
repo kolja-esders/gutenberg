759b72dc60177a882fc4ef891ec6fb3829d6e200
Kolja Esders <kolja.esders@gmail.com>
2018-03-16 03:30:19 +0100

    Simplified profile view

--- client/modules/core/ProfileView/ProfileView.js
+++ client/modules/core/ProfileView/ProfileView.js
@@ -1,193 +1,50 @@
 import Page from 'components/Page/Page';
 import { withAuth } from 'modules/auth/utils';
 import React from 'react';
-import { createFragmentContainer, graphql, commitMutation } from 'react-relay';
+import { createFragmentContainer, graphql } from 'react-relay';
 import PropTypes from 'prop-types';
-import { Label, Modal, Icon, Button, Menu, Segment, Header, Form } from 'semantic-ui-react';
-import Dropzone from 'react-dropzone';
-import { hasValidJwtToken } from 'modules/auth/jwtUtils';
+import { Segment, Label } from 'semantic-ui-react';
 
-import UserAvatar from '../../../components/UserAvatar/UserAvatar';
+import VerticalNav from '../../../components/VerticalNav/VerticalNav';
+import ProfileSettingsTab from '../../../components/ProfileSettingsTab/ProfileSettingsTab';
+import ProfileInvitesTab from '../../../components/ProfileInvitesTab/ProfileInvitesTab';
 
 import styles from './ProfileView.scss';
-
-const UpdateUserMutation = graphql`
-  mutation ProfileView_UpdateUser_Mutation(
-    $userIdInput: ID!, $firstNameInput: String!, $lastNameInput: String!
-  ) {
-    updateUser(userId: $userIdInput, firstName: $firstNameInput, lastName: $lastNameInput) {
-      user {
-        id
-      }
-    }
-  }
-`;
 
 class ProfileView extends React.Component {
   static propTypes = {
     viewer: PropTypes.object.isRequired,
-    relay: PropTypes.object.isRequired
-  }
-  state = { user: Object.assign({}, this.props.viewer.user), droppedFiles: [], success: false, accept: '', files: [], dropzoneActive: false, uploading: false }
-
-  onDragEnter = () => {
-    this.setState({ ...this.state, dropzoneActive: true });
   }
 
-  onDragLeave = () => {
-    this.setState({ ...this.state, dropzoneActive: false });
+  state = { activeItem: 'profile' }
+
+  onItemSelect = (name) => {
+    this.setState({ activeItem: name });
   }
-
-  onDrop = (files) => {
-    this.setState({ ...this.state, files });
-  }
-
-  handleChange = (e, { value }) => {
-    const name = e.target.name;
-    const { user } = this.state;
-    user[name] = value;
-    this.setState({ ...this.state, user });
-  }
-
-  handleSave = () => {
-    const { user } = this.props.viewer;
-    const variables = {
-      userIdInput: user.id,
-      firstNameInput: this.state.user.firstName,
-      lastNameInput: this.state.user.lastName
-    };
-
-    commitMutation(this.props.relay.environment, {
-      mutation: UpdateUserMutation,
-      variables,
-      onCompleted: () => {
-        if (this.state.success) {
-          return;
-        }
-        this.setState({ ...this.state, success: true });
-        setTimeout(() => {
-          this.setState({ ...this.state, success: false });
-        }, 2000);
-      }
-    });
-  }
-
-  // Wrapping of async arrow function needs to happen since 'this' won't be resolved
-  // correctly caused by a bug in react-hot-loader.
-  handleProfileImageSave = () => (async () => {
-    const file = this.state.files[0];
-    window.URL.revokeObjectURL(file.preview);
-
-    this.setState({ ...this.state, uploading: true });
-
-    // Acquire presigned url from backend.
-    let response = await fetch(`/s3/sign?localName=${file.name}`, {
-      method: 'get',
-      credentials: 'same-origin',
-      headers: {
-        Authorization: `Bearer ${hasValidJwtToken().token}`,
-      },
-    });
-    const payload = await response.json();
-
-    // Upload image to S3.
-    response = await fetch(payload.url, {
-      method: 'put',
-      body: file
-    });
-
-    // Register uploaded image with backend.
-    response = await fetch(`/s3/register?assetName=${payload.filename}`, {
-      method: 'get',
-      credentials: 'same-origin',
-      headers: {
-        Authorization: `Bearer ${hasValidJwtToken().token}`,
-      },
-    });
-
-    const filename = payload.filename;
-
-    if (response.status === 200) {
-      const { user } = this.state;
-      user.profileImage = filename;
-      this.setState({ ...this.state, uploading: false, user });
-    }
-  })();
 
   render() {
-    const { user } = this.state;
+    const { activeItem } = this.state;
 
     return (
       <Page title='Gutenberg' viewer={this.props.viewer}>
-        <section className={styles.container}>
-          <Segment padded='very' className={styles.clearfix}>
-            <section className={styles.nav}>
-              <Menu vertical>
-                <Menu.Item name='profile' active>
-                  Profile
-                </Menu.Item>
-
-                <Menu.Item name='invites'>
-                  Open Invites
-                  <Label>0</Label>
-                </Menu.Item>
-              </Menu>
-            </section>
-            <section className={styles.content}>
-              <div className={styles.overlayContainer}>
-                <UserAvatar user={user} size={200} />
-                <Modal
-                  size='small'
-                  dimmer='blurring'
-                  trigger={
-                    <div className={styles.overlayWrap}>
-                      <Icon name='write' size='huge' />
-                    </div>
-                  }
-                >
-                  <Modal.Header>Change Image</Modal.Header>
-                  <Modal.Content>
-                    <div className={styles.clearfix}>
-                      <Dropzone
-                        accept='image/jpeg,image/png'
-                        multiple={false}
-                        onDrop={this.onDrop}
-                        onDragEnter={this.onDragEnter}
-                        onDragLeave={this.onDragLeave}
-                        className={styles.dropzone}
-                      >
-                        { this.state.files.length > 0 ?
-                          <img width='200' className={styles.profileImagePreview} alt='Profile preview' src={this.state.files[0].preview} />
-                        :
-                        this.state.dropzoneActive ? 'Release to drop' : 'Click or drop image here' }
-                      </Dropzone>
-                      <Button className={styles.saveImage} onClick={this.handleProfileImageSave} color='green' loading={this.state.uploading}>Update image</Button>
-                    </div>
-                  </Modal.Content>
-                </Modal>
-              </div>
-              <Header as='h1'>Profile</Header>
-              <Form className={styles.profileInfo}>
-                <Form.Group>
-                  <Form.Input placeholder='First name' label='First name' name='firstName' value={user.firstName} onChange={this.handleChange} />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Input placeholder='Last name' label='Last name' name='lastName' value={user.lastName} onChange={this.handleChange} />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Input placeholder='E-Mail' label='E-Mail' name='email' disabled value={user.email} onChange={this.handleChange} />
-                </Form.Group>
-                { this.state.success ?
-                  <Button color='green'>
-                    <Icon name='check' />Changes saved
-                  </Button>
-                :
-                  <Button onClick={this.handleSave} color='green'>Save changes</Button>
-                }
-              </Form>
-            </section>
+        <div className={styles.container}>
+          <Segment className={styles.segment}>
+            <VerticalNav className={styles.nav}>
+              <VerticalNav.Item name='profile' color='red' active={activeItem === 'profile'} onClick={this.onItemSelect}>
+                Profile
+              </VerticalNav.Item>
+              <VerticalNav.Item name='invites' color='green' active={activeItem === 'invites'} onClick={this.onItemSelect}>
+                Invites
+              </VerticalNav.Item>
+              <VerticalNav.Item name='test' color='blue' active={activeItem === 'test'} onClick={this.onItemSelect}>
+                Test
+              </VerticalNav.Item>
+            </VerticalNav>
+            <div className={styles.content}>
+              <ProfileSettingsTab viewer={this.props.viewer} />
+            </div>
           </Segment>
-        </section>
+        </div>
       </Page>
     );
   }
@@ -196,13 +53,7 @@ class ProfileView extends React.Component {
 export default createFragmentContainer(withAuth(ProfileView), graphql`
   fragment ProfileView_viewer on Viewer {
     ...Page_viewer
-    user {
-      id
-      firstName
-      lastName
-      dateJoined
-      email
-      profileImage
-    }
+    ...ProfileSettingsTab_viewer
+    ...ProfileInvitesTab_viewer
   }
 `);
