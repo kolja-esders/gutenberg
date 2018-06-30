027e91edc0b94e71f6a1561462bdda46b1dfad9d
Kolja Esders <kolja.esders@gmail.com>
2018-07-01 01:57:04 +0200

    Started rework of add book page

--- client/modules/core/AddEditionUserJoin/AddEditionUserJoin.js
+++ client/modules/core/AddEditionUserJoin/AddEditionUserJoin.js
@@ -4,11 +4,10 @@ import DropdownItem from 'components/DropdownItem/DropdownItem'
 import { withAuth } from 'modules/auth//utils';
 import { Input, Dropdown, Button, Rating, Grid, Segment, Header } from 'semantic-ui-react';
 import { graphql, createRefetchContainer } from 'react-relay';
-import createEditionMutation from '../mutations/CreateEdition';
+import createBookAndEditionFromGoodreadsMutation from '../mutations/CreateBookAndEditionFromGoodreads';
 import createEditionUserJoinMutation from '../mutations/CreateEditionUserJoin';
 import styles from './AddEditionUserJoin.scss';
 import URLSearchParams from 'url-search-params'
-
 
 
 const stateOptions = [{ key: 'toread', value: 'toread', text: 'to read' },
@@ -23,195 +22,67 @@ const ratingOptions = [{ key: 1, value: 1, text: '1' },
                         { key: 5, value: 5, text: '5' },
 ];
 
-
-function validateInput(input) {
-  let errors = [];
-  let id = 0;
-
-  input = { ...input };
-
-  if (!input.title) {
-    id++;
-    errors.push({
-      id,
-      key: '',
-      message: 'Please fill out the title field'
-    });
-  }
-  if (!input.author) {
-    id++;
-    errors.push({
-      id,
-      key: '',
-      message: 'Please fill out the author field'
-    });
-  }
-  // if (!input.state) {
-  //   id++;
-  //   errors.push({
-  //     id,
-  //     key: '',
-  //     message: 'Please choose a state'
-  //   });
-  // }
-  // if (!input.rating) {
-  // id++;
-  // errors.push({
-  //   id,
-  //   key: '',
-  //   message: 'Please choose a rating'
-  // });
-  // }
-  if (errors.length === 0) {
-   // Empty array will still return true
-    errors = false;
-  }
-  return { input, errors };
-}
-
 class AddEditionUserJoin extends React.Component {
   state = {
-    input:
-    {
+    input: {
       title: '',
       author: '',
       state: 'to-read',
       rating: 0
     },
-    active: 'to-read',
-    matchAuthors:
-    {
-     key:
-      {
-       title: '',
-       author: ''
-      }
-    },
+    selectedState: 'to-read',
     errors: [],
-    dropDownOpen: false
+    editions: new Map(),
+    editionOptions: [],
+    selectedEditionWorkId: ''
   }
 
-
-  handleTitleFieldChange = (e, { value }) => { // not used
-    console.log('Dropdown titlefield change.');
-
-    const input = this.state.input;
-    const inputName = e.target.id;
-    input[inputName] = e.target.value;
-    this.setState({ ...this.state, input });
-
-    if (this.state.input.title.length >= 3) {
-      const refetchVariables = fragmentVariables => ({
-        title: " ",
-        author: " ",
-        input: this.state.input.title
-      });
-
-      this.props.relay.refetch(refetchVariables, null, this.autoComplete);
+  handleEditionSearchTextChange = async (e, data) => {
+    // Trigger first fetch only when there are at least three characters.
+    if (data.length < 3) {
+      return;
     }
-  }
 
-  handleFieldChange = (e, { value }) => { // not used
-    console.log('Dropdown field change.');
+    const AUTOCOMPLETE_BASE_URL = 'https://www.goodreads.com/book/auto_complete?';
+    const PROXY_URL_PREFIX = 'https://cors-anywhere.herokuapp.com/';
+    const searchParams = new URLSearchParams('format=json');
+    searchParams.set('q', data);
 
-    const input = this.state.input;
-    const inputName = e.target.id;
-    input[inputName] = e.target.value;
-    this.setState({ ...this.state, input });
+    const queryUrl = PROXY_URL_PREFIX + AUTOCOMPLETE_BASE_URL + searchParams.toString();
 
-  }
+    const response = await fetch(queryUrl);
+    const responseJson = await response.json();
 
-  // Dropdown manipulations
-
-  handleDropdownInputChange = (e, data) => {
-    console.log('Dropdown title change.');
-
-    const input = this.state.input;
-    const inputName = "title";
-    input[inputName] = data;
-    const dropDownOpen = true;
-
-    this.setState({ ...this.state, input, dropDownOpen });
-
-    if (this.state.input.title.length >= 3) {
-      const refetchVariables = fragmentVariables => ({
-        title: " ",
-        author: " ",
-        input: this.state.input.title
-      });
-      this.props.relay.refetch(refetchVariables, null, this.autoComplete);
+    const editions = new Map();
+    for (const x of responseJson) {
+      editions.set(x.workId, x);
     }
+
+    const editionOptions = responseJson.map(x => ({
+      key: x.workId,
+      value: x.workId,
+      text: x.bookTitleBare,
+      content: <DropdownItem
+        bookImage={x.imageUrl}
+        bookTitle={x.bookTitleBare}
+        bookAuthor={x.author.name}
+        onClick={this.handleDropdownSelection}
+      />
+    }));
+
+    this.setState({ ...this.state, editionOptions, editions });
   }
 
-  handleDropdownClick = (e, data) => {
-    console.log('Dropdown click.');
-
-    const dropDownOpen = true;
-
-    this.setState({...this.state, dropDownOpen});
+  handleEditionChange = (_, data) => {
+    this.setState({ ...this.state, selectedEditionWorkId: data.value });
   }
 
-  handleFieldChange = (e,data) => { // not used
-    console.log('Dropdown open.');
-
-    const dropDownOpen = true;
-
-    this.setState({...this.state, dropDownOpen});
-  }
-
-  handleDropdownOpen = (e,data) => {
-    console.log('Dropdown open.');
-
-    const dropDownOpen = true;
-
-    this.setState({...this.state, dropDownOpen});
-  }
-
-  handleDropdownClose = (e,data) => {
-    console.log('Dropdown close.');
-
-    const dropDownOpen = false;
-
-    this.setState({...this.state, dropDownOpen});
-  }
-
-  handleAuthorDropdownChange = (e, data) => {
-    const input = this.state.input;
-    const inputName = "author";
-    input[inputName] = data.value;
-    this.setState({ ...this.state, input });
-  }
-
-
-  handleDropdownFinalChange = (e, data) => {
-    console.log('Dropdown final change.')
-    console.log(data)
-    console.log(this.props.viewer)
-
-    const input = this.state.input;
-    const dropDownOpen = true;
-
-    input["author"] = this.state.matchAuthors[data.value].author;
-    input["title"] = this.state.matchAuthors[data.value].title;
-    this.setState({ ...this.state, input, dropDownOpen  });
-  }
-
-  handleDropdownSelection = (e,data) => {
-    console.log(data);
-    console.log('Dropdown selection.');
-
-    const dropDownOpen = false;
-
-    this.setState({...this.state, dropDownOpen});
-  }
-
-  handleButtonChange = (e, data) => {
+  handleStateChange = (e, data) => {
     e.preventDefault();
     const input = this.state.input;
     const inputName = data.type;
     input[inputName] = data.id;
-    this.setState({ ...this.state, input });
-    this.setState({ ...this.state, active: data.id });
+    this.setState({ ...this.state, input, selectedState: data.id });
   }
 
   handleRatingChange = (e, data) => {
@@ -221,45 +92,30 @@ class AddEditionUserJoin extends React.Component {
     this.setState({ ...this.state, input });
   }
 
-
   setErrors = (errors) => {
     this.setState({ ...this.state, errors });
   }
 
-
-  onCompletedSubmit = (ev) => {
+  handleSubmit = (ev) => {
     ev.preventDefault();
-    console.log("onCompleted")
-    console.log(this.state.input)
-    const { input, errors } = validateInput(this.state.input);
-    if (errors) {
-      this.setErrors(errors);
+    if (!this.state.editions.has(this.state.selectedEditionWorkId)) {
+      // TODO: Push to errors
+      console.log('No edition has been selected');
       return;
     }
+    const { environment } = this.props.relay;
+    const edition = this.state.editions.get(this.state.selectedEditionWorkId);
 
-    //Check if book already exists
-    const refetchVariables = fragmentVariables => ({
-      title: input.title,
-      author: input.author,
-      input: input.title
-    });
-
-
-    this.props.relay.refetch(refetchVariables, null, this.checkIfBookExists);
-  }
-
-  checkIfBookExists = (error) => {
-    if(this.props.viewer.book == null){
-      const variables = {
-        titleInput: this.state.input.title,
-        authorInput: this.state.input.author,
-      };
-      createEditionMutation(this.props.relay.environment, variables, this.onCompletedCreateEdition, this.setErrors);
-
-    } else {
-      console.log("book already exists")
-      this.onCompletedRefetch();
-    }
+    // TODO: Check (maybe based on platform workId for now) whether book already exists.
+    const variables = {
+      titleInput: edition.bookTitleBare,
+      authorIdInput: '',
+      goodreadsWorkUidInput: edition.workId,
+      goodreadsBookUidInput: edition.bookId
+    };
+    createBookAndEditionFromGoodreadsMutation(environment, variables, (error, data) => {
+      console.log(error, data);
+    }, this.setErrors);
   }
 
   onCompletedCreateEdition = (error, data) => {
@@ -276,10 +132,8 @@ class AddEditionUserJoin extends React.Component {
       console.error(error);
       return;
     }
-    console.log("CompletedRefetch")
 
-
-    var { rating, state } = this.state.input;
+    let { rating, state } = this.state.input;
     const { book, user } = this.props.viewer;
 
     if (state != "read") {
@@ -295,8 +149,18 @@ class AddEditionUserJoin extends React.Component {
       ratingInput: rating,
       stateInput: state
     };
-    console.log(variables)
-    console.log("create")
+
+    //const variables = {
+      //title: user.id,
+      //goodreadsAuthorUid: user.id,
+      //goodreadsBookUid: book.id,
+      //goodreadsWorkUid: rating,
+    //};
+
+    //createAuthorFromGoodreads(this.props.relay.environment, variables, this.onCompletedEditionUserJoin, this.setErrors);
+
+    //createBookAndEditionFromGoodreads(this.props.relay.environment, variables, this.onCompletedEditionUserJoin, this.setErrors);
+
     createEditionUserJoinMutation(this.props.relay.environment, variables, this.onCompletedEditionUserJoin, this.setErrors);
   }
 
@@ -312,101 +176,8 @@ class AddEditionUserJoin extends React.Component {
     return [];
   }
 
-//intermediary solution
-setArguments = (bookTitle, authorName) => {
-  const input = this.state.input
-  input['author']  = authorName
-  input['title']  = bookTitle
-  this.setState({ ...this.state, input})
-
-}
-
-  autoComplete = () => {
-    console.log("autoComplete")
-
-    // Fetch data from Goodreads
-    // const url = new URL('https://www.goodreads.com/book/auto_complete?format=json&q=dan+ariely')
-    // const url = new URL('https://www.goodreads.com/book/auto_complete');
-    let baseurl = 'https://www.goodreads.com/book/auto_complete?';
-    const params = new URLSearchParams('format=json');
-    params.set('q', this.state.input.title);
-    let finalurl = baseurl.concat(params.toString());
-    let proxyurl = 'https://cors-anywhere.herokuapp.com/';
-
-    let result;
-
-    // console.log(finalurl);
-    // console.log(params.toString());
-    console.log(proxyurl + finalurl);
-
-    fetch(proxyurl + finalurl)
-    .then(response => response.json()//{
-      // console.log(response.json());
-      // const bookdata = response.json()
-        .then(data => {
-          console.log(data)
-          console.log(data[0].bookTitleBare)
-          console.log(data[0].author.name)
-
-          this.setArguments(data[0].bookTitleBare, data[0].author.name)
-
-          const bookOptions = data.map((x) => (
-            {
-              key: x.bookId,
-              value: x.bookId,
-              text: x.bookTitleBare,
-              // text: x.bookTitleBare + " by " + x.author.name
-              content: <DropdownItem
-                bookImage = {x.imageUrl}
-                bookTitle = {x.bookTitleBare}
-                bookAuthor = {x.author.name}
-                onClick = {this.handleDropdownSelection}
-                />
-                /*<Header
-                 image={x.imageUrl}
-                 content={x.bookTitleBare}
-                 subheader={x.author.name}
-                 onClick={this.handleDropdownSelection}
-                />*/
-            }))
-
-          this.setState({ ...this.state, bookOptions });
-        })
-        .catch(() => console.log("Error"))
-    )
-
-      // update dropdown
-
-    // .then(contents => console.log(contents))
-    // .then(contents => result)
-    .catch(() => console.log("Can't access " + finalurl + " response."))
-
-    // console.log(result);
-
-    // const bookOptions = this.props.viewer.booksAutocompleted.map((x) => ({key: x.id, value: x.id, text: x.title+" by "+x.author}))
-
-    /*for (var i=0; i < this.props.viewer.booksAutocompleted.length; i++){
-      const matchAuthors = this.state.matchAuthors;
-      const key = this.props.viewer.booksAutocompleted[i].id;
-      matchAuthors[key] = this.props.viewer.booksAutocompleted[i];
-
-      this.setState({ ...this.state, matchAuthors});
-    };
-  */
-  //  this.setState({ ...this.state, bookOptions });
-  }
-
-  /*autoComplete2 = () => {
-    console.log("autoComplete2")
-
-    const bookOptions = this.props.viewer.booksAutocompleted.map((x) => ({key: x.id, value: x.id, text: x.title+" by "+x.author});
-
-    this.setState({ ...this.state, bookOptions });
-  }*/
-
 
   render() {
-
     const { input, erros } = this.state;
     const title = 'Add Book to Bookshelf';
 
@@ -422,25 +193,19 @@ setArguments = (bookTitle, authorName) => {
             <Dropdown
                  id="title"
                  className={styles.nameField}
-                 options={this.state.bookOptions}
-                 search
+                 options={this.state.editionOptions}
                  selection
-                 fluid
-                 placeholder='book title'
-                 open={this.state.dropDownOpen}
-                 onSearchChange={this.handleDropdownInputChange}
-                 onChange={this.handleDropdownFinalChange}
-                 onClick={this.state.handleDropdownClick}
-                 // onOpen={this.state.handleFieldChange}
-                 onOpen={this.state.handleDropdownOpen}
-                 onClose={this.state.handleDropdownClose}
+                 search
+                 placeholder='E.g. "Harry Potter"'
+                 onSearchChange={this.handleEditionSearchTextChange}
+                 onChange={this.handleEditionChange}
                />
 
 
           <Button.Group className={styles.readingStatus} widths='3' basic>
               <Button type='state'
-                onClick={this.handleButtonChange}
-                active={this.state.active == "to-read"}
+                onClick={this.handleStateChange}
+                active={this.state.selectedState == "to-read"}
                 id="to-read"
                 className={styles.stateButton}>
                 <div className={styles.toReadIcon} />
@@ -448,8 +213,8 @@ setArguments = (bookTitle, authorName) => {
               </Button>
 
               <Button type='state'
-                onClick={this.handleButtonChange}
-                active={this.state.active == "reading"}
+                onClick={this.handleStateChange}
+                active={this.state.selectedState == "reading"}
                 id="reading"
                 className={styles.stateButton}>
                 <div className={styles.readingIcon} />
@@ -457,8 +222,8 @@ setArguments = (bookTitle, authorName) => {
               </Button>
 
               <Button type='state'
-                onClick={this.handleButtonChange}
-                active={this.state.active == "read"}
+                onClick={this.handleStateChange}
+                active={this.state.selectedState == "read"}
                 id="read"
                 className={styles.stateButton}>
                 <div className={styles.readIcon} />
@@ -466,7 +231,7 @@ setArguments = (bookTitle, authorName) => {
               </Button>
             </Button.Group>
 
-            <div className={styles.ratingContainer} hidden={this.state.active != "read"}>
+            <div className={styles.ratingContainer} hidden={this.state.selectedState != "read"}>
               <span>Your rating:</span>
               <Rating maxRating={5}
                 onRate={this.handleRatingChange}
@@ -480,7 +245,7 @@ setArguments = (bookTitle, authorName) => {
               fluid
               type='submit'
               size='huge'
-              onClick = {this.onCompletedSubmit}
+              onClick = {this.handleSubmit}
               className='button_submit-add-books-form'
             >
               Add book
@@ -504,40 +269,30 @@ export default createRefetchContainer(
         title: {type: "String"},
         author: {type: "String"},
         input: {type: "String"}
-      ){
+      ) {
         ...Page_viewer
         book(title: $title, author: $author) {
           id
         }
-        user{
+        user {
           id
-        }
-
-        booksAutocompleted(title: $input){
-          id
-          title
-          author
         }
       }
-      `,
-
+    `,
     user: graphql`
      fragment AddEditionUserJoin_user on Viewer {
-          user{
+          user {
            id
          }
        }
     `,
   },
-
-
   graphql`
     query AddEditionUserJoinRefetchQuery($title: String!, $author: String!, $input: String!){
       viewer {
         ...AddEditionUserJoin_viewer @arguments(title: $title, author: $author, input: $input)
         ...AddEditionUserJoin_user
-
       }
     }
-    `,
+  `,
 );
